df07fbe2e4c5c4936d6f71ab6de123c09b53cc82
Rachel Fish <itsrachelfish@gmail.com>
2014-12-08 23:47:56 -0700

    Updated websockets to use separate events folder

--- src/server.js
+++ src/server.js
@@ -74,51 +74,22 @@ function random_int(min, max)
 io.sockets.on('connection', function(socket)
 {
     var user = {};
+    var channels = {};
+
+    // Required variables events need access to
+    var required = {socket: socket, user: user, channels: channels};
+    var events = ['join'] //, 'chat', 'video', 'time', 'disconnect'];
+
+    events.map(function(event)
+    {
+        require(__dirname + '/events/'+event+'.js')(required);
+    });
     
     console.log('User connected');
     socket.emit('connected');
     
     count++;
     io.sockets.emit('stats', {users: count, channels: Object.keys(channels).length});
-
-    socket.on('join', function(channel)
-    {
-        // Is this dangerous?
-        //socket.join(channel);
-
-        // Maybe this is a better idea...
-        channel = channel.toString();
-        user.channel = channel;
-        user.color = {h: random_int(0, 360), s: random_int(25, 100), l: random_int(25, 50)};
-
-        socket.join(channel);
-
-        if(typeof channels[channel] == "undefined")
-        {
-            // We should probably store the leader's session ID?
-            channels[channel] = {users: 0, leader: false, video: {}};
-        }
-
-        // If there is no leader, you get to be a leader!
-        if(!channels[channel].leader)
-        {
-            channels[channel].leader = true;
-
-            // TODO: Leadership should be based on channel, in case a user joins multiple?
-            user.leader = true;
-        }
-
-        channels[channel].users++;
-
-        // Tell the user about itself
-        socket.emit('join', user);
-        socket.emit('time', channels[channel].video);
-
-        // Emit other statistics
-        io.sockets.emit('stats', {channels: Object.keys(channels).length});
-        io.to(channel).emit('stats', {channel: channels[channel].users});
-        console.log("User joined "+channel);
-    });
     
     socket.on('chat', function(chat)
     {
