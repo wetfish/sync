0bf486fdb04afbf8145eafed38c345a33c1b76f8
Rachel Fish <itsrachelfish@gmail.com>
2014-12-13 23:59:55 -0700

    Basic post request handler

--- src/routes/channel/create.js
+++ src/routes/channel/create.js
@@ -8,8 +8,6 @@ module.exports = function(required)
 
     app.get('/create', function(req, res)
     {
-        req.session.destroy();
-        
         console.log("GET: /create");
         res.render('channel/create', {
             session: req.session,
@@ -19,5 +17,11 @@ module.exports = function(required)
                 foot: 'partials/foot'
             }
         });
+    });
+
+    app.post('/create', function(req, res)
+    {
+        console.log("GET: /create");
+        res.end(JSON.stringify(req.query, null, 4));
     });
 }
