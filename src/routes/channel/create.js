f6f3e14b93b302fc592311863ff8d2d5d1a2027a
Rachel Fish <itsrachelfish@gmail.com>
2014-12-14 23:09:31 -0700

    Added error message if you try to create without being logged in

--- src/routes/channel/create.js
+++ src/routes/channel/create.js
@@ -1,3 +1,19 @@
+var events = require('events')
+var action = new events.EventEmitter();
+
+action.on('render', function(req, res, data)
+{
+    res.render('channel/create', {
+        session: req.session,
+        alert: data.alert,
+        partials: {
+            head: 'partials/head',
+            header: 'partials/header',
+            foot: 'partials/foot'
+        }
+    });
+});
+
 var app = false;
 var model = false;
 
@@ -9,14 +25,16 @@ module.exports = function(required)
     app.get('/create', function(req, res)
     {
         console.log("GET: /create");
-        res.render('channel/create', {
-            session: req.session,
-            partials: {
-                head: 'partials/head',
-                header: 'partials/header',
-                foot: 'partials/foot'
-            }
-        });
+
+        if(typeof req.session.login == "undefined")
+        {
+            var alert = {class: 'alert', message: 'You must login to create a channel.'};
+            action.emit('render', req, res, {alert: alert});
+        }
+        else
+        {
+            action.emit('render', req, res);
+        }
     });
 
     app.post('/create', function(req, res)
