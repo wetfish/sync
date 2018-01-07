70c9186947ca7074d2c97eab581a689c0b9c4216
JonathanSchwob <jonathanrschwob@gmail.com>
2018-01-07 13:53:32 -0800

    establish ws connection, add react-ws

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Websocket from 'react-websocket';
 
-class App extends Component {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props); 
+    this.state = {
+      connected: 'false'
+    };
+  }
+
+  handleOpen(data) {
+    this.setState({connected: 'true'}, () => {
+      console.log('state set');
+    });
+  }
+
+  handleClose(data) {
+    this.setState({connected: 'false'}, () => {
+      console.log('state set');
+    });
+  }
+  
+  handleData(data) {
+    console.log('todo');
+  }
+
   render() {
     return (
-      <div className="App">
-        sync
+      <div>
+        Connected: <strong>{this.state.connected}</strong>
+
+        <Websocket url='ws://localhost:3001/echo'
+          onMessage={this.handleData.bind(this)}
+          onOpen={this.handleOpen.bind(this)}
+          onClose={this.handleClose.bind(this)}/>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-var express = require('express');
+const express = require('express');
+const enableWs = require('express-ws');
 var path = require('path');
-var app = express();
+const app = express();
+
+enableWs(app);
 
 app.use(express.static(path.join(__dirname, 'client')));
+
+app.ws('/echo', (ws, req) => {
+  ws.on('message', msg => {
+    ws.send(msg);
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket was closed');
+  });
+});
 
 app.listen(3001, function() {
   console.log('listening on port 3001');
 });
+
