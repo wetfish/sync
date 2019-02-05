9db175ea41cee5d6f74a759aff59c922df7fad38
tmccurbin <tmccurbin@gmail.com>
2019-02-05 01:37:53 -0500

    Changed playlist from external files to local files

--- media-player.js
+++ media-player.js
@@ -1,21 +1,11 @@
 let playlist = [
-    'https://www.youtube.com/watch?v=lvOTaXIog6k', // Main video
-    'https://www.youtube.com/embed/lvOTaXIog6k', // Embedded video
-    'https://vimeo.com/90049558', // Main video
-    'https://player.vimeo.com/video/90049558', // Embedded video
-    'https://www.twitch.tv/videos/212659577', // Main video
-    'https://player.twitch.tv/?autoplay=false&video=v212659577', // Embedded video
-    'https://soundcloud.com/calebxbelkin/paradise', // Main audio player
-    'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/244762996', // Embedded audio player 
-    'https://open.spotify.com/album/3psdlpnB9inNvASEQfbHkQ', // Main audio player (album)
-    'https://open.spotify.com/track/1cd8bP4mdFZyRJ5daFg5V2', // Main audio player (song)
-    'https://open.spotify.com/embed/track/1cd8bP4mdFZyRJ5daFg5V2', // Embedded audio player (song)
-    'https://one.npr.org/?sharedMediaId=689947046:689947510', // Main audio player
-    'https://play.podtrac.com/npr-510306/npr.mc.tritondigital.com/NPR_510306/media/anon.npr-mp3/npr/tinydesk/2019/01/20190125_tinydesk_bloodorangepodcast.mp3', // RSS link
-    'https://www.stitcher.com/podcast/national-public-radio/hidden-brain/e/58186563', // Main audio player
-    'https://app.stitcher.com/splayer/f/71231/58186563', // Embedded audio player
-    'https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/episode/the-legend-of-betsy-ross-30460525/', // Main audio player
-    'https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/episode/the-legend-of-betsy-ross-30460525/?embed=true/' // Embedded audio
+    './media_files/giant-steps.ogv',                // Web video
+    './media_files/have-you-met-miss-jones.ogg',    // Web audio
+    './media_files/milestones.flac',                // Lossless audio
+    './media_files/nows-the-time.mp4',              // Video
+    './media_files/sing-sing-sing.webm',            // Web video
+    './media_files/so-what.mp3',                    // Audio
+    './media_files/straight-no-chaser.wav'          // Audio
 ]
 
 module.exports = {
