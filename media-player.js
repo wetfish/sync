c65de7cc1f1361f22a8499af54e1d52723e1febc
tmccurbin <tmccurbin@gmail.com>
2019-02-27 23:27:27 -0500

    Server can roughly distinguish between audio and video

--- media-player.js
+++ media-player.js
@@ -10,9 +10,23 @@ let playlist = [
 
 let startTime;
 let mediaIndex = 0;
+let mediaTypes = [];
 let filesProcessed = 0;
 let breakpoints = new Array(playlist.length);
 let mediaLengths = new Array(playlist.length);
+let videoTypes = new Set(['mkv', 'flv', 'f4a', 'f4b', 'f4p', 'f4v', 'vob', 'ogv', 'drc', 'gif', 'ifv', 'mng', 'avi', 'mts', '2ts', 'mov', '.qt', 'wmv', 'yuv', '.rm', 'mvb', 'mp4', 'm4v', 'mpg', 'mp2', 'peg', 'mpe', 'mpv', 'm2v', 'svi', '3g2', 'mxf', 'roq', 'nsv']);
+let audioTypes = new Set(['.aa', 'aac', 'aax', 'act', 'iff', 'amr', 'ape', '.au', 'awb', 'dct', 'dss', 'dvf', 'lac', 'gsm', 'lax', 'ivs', 'm4a', 'mmf', 'mp3', 'mpc', 'msv', 'nmf', 'nsf', 'oga', 'pus', '.ra', '.rm', 'raw', 'sln', 'tta', 'vox', 'wav', 'wma', '.wv', 'svx']);
+let ambiguousTypes = new Set(['ogg', 'm4p', '3gp', 'ebm']); // These can be either audio or video
+
+const getMediaTypes = function() {
+    playlist.forEach((url) => {
+        let extension = url.slice(-3).toLowerCase();
+        if (audioTypes.has(extension)) mediaTypes.push('audio');
+        else if (videoTypes.has(extension)) mediaTypes.push('video');
+        else if (ambiguousTypes.has(extension)) mediaTypes.push('ambiguous');
+        else mediaTypes.push('unknown');
+    });
+};
 
 const setBreakpoints = function() {
     let totalTime = 0;
@@ -55,6 +69,7 @@ const getMediaLength = function (url, index) {
 
 // Get mediaLengths. Start timer
 const init = function() {
+    getMediaTypes();
     playlist.forEach((fileUrl, index) => {
         getMediaLength(fileUrl, index);
     });
