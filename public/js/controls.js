06a6959a666a1ef2acd3a45d4063daa819f7f374
Meleeman01 <meleeman01@live.com>
2022-01-12 11:42:42 -0700

    fixed pointer not disappearing on slider

--- public/js/controls.js
+++ public/js/controls.js
@@ -2,17 +2,21 @@ window.addEventListener('DOMContentLoaded', () => {
     // Implement controls for shwing and hiding the progress bar
     // Inspired by https://blog.videojs.com/hiding-and-showing-video-player-controls/
 
-    let controls = document.getElementById('controls-container');
-    let cursor = document.querySelector('body');
+    const controls = document.getElementById('controls-container');
+    const cursor = document.querySelector('body');
+    const volume = document.querySelector('.slider');
     let startTimer;
 
     document.addEventListener('mousemove', function(){
         clearTimeout(startTimer);
         controls.classList.add('active');
         cursor.style.cursor = 'default';
+        //we need to turn off pointer events for the slider so it disappears when the mouse is hovered on it
+        volume.classList.add('slider-active');
         startTimer = setTimeout(() => {
             console.log("removing");
             controls.classList.remove('active');
+            volume.classList.remove('slider-active');
             cursor.style.cursor = 'none';  
         }, 4000);
     });
